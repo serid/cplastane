3597a45e3ed8ba9460eac9d17785d0286a25c200
Sergey <serridd@gmail.com>
2021-01-10 14:16:44 +0300

    Upgrade testing code to support checking bytes of assembly output for correctness and add a test for `push/pop`.

--- test/assembly_test.cxx
+++ test/assembly_test.cxx
@@ -8,9 +8,19 @@
 using assembly::mnemo_t;
 
 namespace test {
+    /// Test options. Combine with bitwise-or
+    enum test_opt_t {
+        NONE = 0,
+        CHECK_BYTES = 0b1, // Check assembly result against expected_bytes.
+        RUN = 0b10, // Run assembly result.
+        CHECK_EXEC_RESULT = 0b100, // Check result of running against expected_exec_result. Implies RUN.
+    };
+
     struct test_t {
         string name;
-        i64 expected_result;
+        u8 test_opt;
+        i64 expected_exec_result;
+        vector<u8> expected_bytes;
         vector<mnemo_t> mnemos;
     };
 
@@ -19,30 +29,56 @@ namespace test {
 
         vector<u8> bytes = assemble(x_test.mnemos);
 
-        std::cout << "Compilation successful, bytes: ";
+        if ((x_test.test_opt & CHECK_BYTES) != 0) {
+            if (bytes == x_test.expected_bytes) {
+                std::cout << "[+] Bytes check successful.\n";
+                std::cout << "Bytes: [";
+                for (const u8 &n:bytes) {
+                    std::cout << std::hex << i32(n >> 4) << i32(n & 0x0f) << std::dec << ", ";
+                }
+                std::cout << "].\n";
+            } else {
+                std::cout << "[/] Bytes check failed.\n";
+                std::cout << "Expected bytes: [";
+                for (const u8 &n:x_test.expected_bytes) {
+                    std::cout << std::hex << i32(n >> 4) << i32(n & 0x0f) << std::dec << ", ";
+                }
+                std::cout << "].\n";
 
-        std::cout << "[";
-        for (const u8 &n:bytes) {
-            std::cout << std::hex << i32(n) << std::dec << ", ";
-        }
-        std::cout << "]\n";
+                std::cout << "Actual bytes: [";
+                for (const u8 &n:bytes) {
+                    std::cout << std::hex << i32(n >> 4) << i32(n & 0x0f) << std::dec << ", ";
+                }
+                std::cout << "].\n";
 
-        i64 result = jit::eval_mc(bytes.data(), bytes.size());
-        if (result != x_test.expected_result) {
-            std::cout << "Test failed.\nExpected result: " << x_test.expected_result
-                      << "\nActual result: " << result << "\n";
-            return false;
-        } else {
-            std::cout << "Success. Result: (" << result << ").\n";
-            return true;
+                return false;
+            }
         }
+
+        if ((x_test.test_opt & RUN) != 0) {
+            i64 result = jit::eval_mc(bytes.data(), bytes.size());
+
+            if ((x_test.test_opt & CHECK_EXEC_RESULT) != 0) {
+                if (result == x_test.expected_exec_result) {
+                    std::cout << "[+] Result check successful.\n";
+                    std::cout << "Result: (" << result << ").\n";
+                    return true;
+                } else {
+                    std::cout << "[/] Result check failed.\n";
+                    std::cout << "Expected result: (" << x_test.expected_exec_result << ").\n"
+                              << "Actual result: (" << result << ").\n";
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     auto test_jit() -> void {
         vector<test_t> tests = {
                 // mov eax, 100
                 // ret
-                {.name="Simple `return 100;`", .expected_result=100, .mnemos={
+                {.name="Simple `return 100;`", .test_opt=RUN | CHECK_EXEC_RESULT, .expected_exec_result=100, .mnemos={
                         {
                                 .tag = mnemo_t::tag_t::Mov,
                                 .width = mnemo_t::width_t::Dword,
@@ -65,7 +101,7 @@ namespace test {
                 // mov ecx, 0xff00
                 // mov eax, ecx
                 // ret
-                {.name="Reg mov test", .expected_result=0xff00, .mnemos={
+                {.name="Reg mov test", .test_opt=RUN | CHECK_EXEC_RESULT, .expected_exec_result=0xff00, .mnemos={
                         {
                                 .tag = mnemo_t::tag_t::Mov,
                                 .width = mnemo_t::width_t::Dword,
@@ -101,7 +137,8 @@ namespace test {
                 // mov rcx, 0x0f1f2f3f4f5f6f7f
                 // mov rax, rcx
                 // ret
-                {.name="64 bit reg mov test", .expected_result=0x0f1f2f3f4f5f6f7f, .mnemos={
+                {.name="64 bit reg mov test", .test_opt=RUN |
+                                                        CHECK_EXEC_RESULT, .expected_exec_result=0x0f1f2f3f4f5f6f7f, .mnemos={
                         {
                                 {
                                         .tag = mnemo_t::tag_t::Mov,
@@ -139,7 +176,8 @@ namespace test {
                 // mov rax, 0x0f0f0f0f0f0f0f0f
                 // mov al, 0
                 // ret
-                {.name="Byte-wise mov", .expected_result=0x0f0f0f0f0f0f0f00, .mnemos={
+                {.name="Byte-wise mov", .test_opt=RUN |
+                                                  CHECK_EXEC_RESULT, .expected_exec_result=0x0f0f0f0f0f0f0f00, .mnemos={
                         {
                                 .tag = mnemo_t::tag_t::Mov,
                                 .width = mnemo_t::width_t::Qword,
@@ -177,7 +215,8 @@ namespace test {
                 // mov dx, cx
                 // mov ax, dx
                 // ret
-                {.name="Byte-wise `mov reg, reg`", .expected_result=0x0f0f0f0f0f0f0a0a, .mnemos={
+                {.name="Byte-wise `mov reg, reg`", .test_opt=RUN |
+                                                             CHECK_EXEC_RESULT, .expected_exec_result=0x0f0f0f0f0f0f0a0a, .mnemos={
                         {
                                 .tag = mnemo_t::tag_t::Mov,
                                 .width = mnemo_t::width_t::Qword,
@@ -246,7 +285,8 @@ namespace test {
                 // mov rax, [rbp - 8]
                 // mov rbp, rcx ; restore rbp
                 // ret
-                {.name="`mov mem reg` without SIB", .expected_result=0x0f0f0f0f0f0f0f00, .mnemos={
+                {.name="`mov mem reg` without SIB", .test_opt=RUN |
+                                                              CHECK_EXEC_RESULT, .expected_exec_result=0x0f0f0f0f0f0f0f00, .mnemos={
                         {
                                 .tag = mnemo_t::tag_t::Mov,
                                 .width = mnemo_t::width_t::Qword,
@@ -358,7 +398,8 @@ namespace test {
                 // mov [rsp - 8], al
                 // mov rax, [rsp - 8]
                 // ret
-                {.name="`mov mem reg` with SIB", .expected_result=0x0f0f0f0f0f0f0f00, .mnemos={
+                {.name="`mov mem reg` with SIB", .test_opt=RUN |
+                                                           CHECK_EXEC_RESULT, .expected_exec_result=0x0f0f0f0f0f0f0f00, .mnemos={
                         {
                                 .tag = mnemo_t::tag_t::Mov,
                                 .width = mnemo_t::width_t::Qword,
@@ -432,7 +473,8 @@ namespace test {
                 // push rax
                 // pop rax
                 // ret
-                {.name="`push/pop`", .expected_result=0x0f0f0f0f0f0f0f00, .mnemos={
+                {.name="`push/pop`", .test_opt=RUN |
+                                               CHECK_EXEC_RESULT, .expected_exec_result=0x0f0f0f0f0f0f0f00, .mnemos={
                         {
                                 .tag = mnemo_t::tag_t::Mov,
                                 .width = mnemo_t::width_t::Qword,
@@ -465,6 +507,69 @@ namespace test {
                                 .tag = mnemo_t::tag_t::Ret,
                                 .width = mnemo_t::width_t::NotSet,
                         }
+                }},
+
+                // push rbx
+                // push 100
+                // push 1000
+                // push [esi+eax*4-10]
+                // pop rbx
+                // pop [esi+eax*4-10]
+                // no ret
+                {.name="`push/pop` bytes multitest", .test_opt=CHECK_BYTES, .expected_bytes={0x53, 0x6a, 0x64, 0x68,
+                                                                                             0xe8, 0x03, 0x00, 0x00,
+                                                                                             0x67, 0xff, 0x74, 0x86,
+                                                                                             0xf6, 0x5b, 0x67, 0x8f,
+                                                                                             0x44, 0x86,
+                                                                                             0xf6}, .mnemos={
+                        {
+                                .tag = mnemo_t::tag_t::Push,
+                                .width = mnemo_t::width_t::Qword,
+                                .a1 = {
+                                        .tag = mnemo_t::arg_t::tag_t::Register,
+                                        .data = {.reg = mnemo_t::arg_t::reg_t::Rbx}
+                                },
+                        },
+                        {
+                                .tag = mnemo_t::tag_t::Push,
+                                .width = mnemo_t::width_t::Byte,
+                                .a1 = {
+                                        .tag = mnemo_t::arg_t::tag_t::Immediate,
+                                        .data = {.imm = 100}
+                                },
+                        },
+                        {
+                                .tag = mnemo_t::tag_t::Push,
+                                .width = mnemo_t::width_t::Dword,
+                                .a1 = {
+                                        .tag = mnemo_t::arg_t::tag_t::Immediate,
+                                        .data = {.imm = 1000}
+                                },
+                        },
+                        {
+                                .tag = mnemo_t::tag_t::Push,
+                                .width = mnemo_t::width_t::Qword,
+                                .a1 = {
+                                        .tag = mnemo_t::arg_t::tag_t::Memory,
+                                        .data = {.memory = {.base=mnemo_t::arg_t::reg_t::Esi, .index=mnemo_t::arg_t::reg_t::Eax, .scale=mnemo_t::arg_t::memory_t::scale_t::S4, .disp=-10}}
+                                },
+                        },
+                        {
+                                .tag = mnemo_t::tag_t::Pop,
+                                .width = mnemo_t::width_t::Qword,
+                                .a1 = {
+                                        .tag = mnemo_t::arg_t::tag_t::Register,
+                                        .data = {.reg = mnemo_t::arg_t::reg_t::Rbx}
+                                },
+                        },
+                        {
+                                .tag = mnemo_t::tag_t::Pop,
+                                .width = mnemo_t::width_t::Qword,
+                                .a1 = {
+                                        .tag = mnemo_t::arg_t::tag_t::Memory,
+                                        .data = {.memory = {.base=mnemo_t::arg_t::reg_t::Esi, .index=mnemo_t::arg_t::reg_t::Eax, .scale=mnemo_t::arg_t::memory_t::scale_t::S4, .disp=-10}}
+                                },
+                        },
                 }},
         };
 
