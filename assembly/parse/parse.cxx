20d964df8ee5624e4c1bb512520e31c06f7ccb31
Sergey <serridd@gmail.com>
2021-03-28 03:04:43 +0300

    Full-fledged assembly parser. (moderately exciting)

--- assembly/parse/parse.cxx
+++ assembly/parse/parse.cxx
@@ -104,99 +104,76 @@ static auto parse_arg(strive tail) -> OptionParserResult<arg_t> {
     // eax
     // [eax * 2 + 100]
 
-    OptionParserResult<arg_t> result =
-            parse_i64(tail).bind<ParserResult<arg_t>>([](ParserResult<i64> result1) {
-                strive tail1 = result1.tail;
-                arg_t result = arg_t::imm(result1.data);
-                return make_option(ParserResult(tail1, result));
-            }).choice([tail]() {
-                return parse_register(tail).bind<ParserResult<arg_t>>([](ParserResult<reg_t> result1) {
-                    strive tail1 = result1.tail;
-                    arg_t result = arg_t::reg(result1.data);
-                    return make_option(ParserResult(tail1, result));
-                });
-            }).choice([tail]() {
-                /* // A (failed) attempt to write parsing code using proper bindings
-                return consume_prefix_char<monostate>(tail, '[', monostate()).bind<ParserResult<arg_t>>(
-                        [](ParserResult<monostate> result1) {
-                            strive tail1 = result1.tail;
-                            return parse_register(tail1).bind<ParserResult<arg_t>>([](ParserResult<reg_t> result2) {
-                                strive tail2 = result2.tail;
-                                reg_t base = result2.data;
-                                reg_t index = result2.data;
+    return parse_i64(tail).bind<ParserResult<arg_t>>([](ParserResult<i64> a) {
+        return make_option(ParserResult(a.tail, arg_t::imm(a.data)));
+    }).choice([tail]() {
+        return parse_register(tail).bind<ParserResult<arg_t>>([](ParserResult<reg_t> a) {
+            return make_option(ParserResult(a.tail, arg_t::reg(a.data)));
+        });
+    }).choice([tail]() {
+        // Parse a memory operand
+        if (OptionParserResult<monostate> a = consume_prefix_char(tail, '[', monostate())) {
+            if (OptionParserResult<reg_t> b = parse_register(a.value().tail)) {
+                reg_t base = b.value().data;
 
-                                arg_t result = {
-                                        .tag = arg_t::tag_t::Register,
-                                        .data = {.reg = result2.data}
-                                };
-                                return make_option(ParserResult(tail2, result));
-                            });
-                        });
-                */
+                // Maybe parse an index with scale
+                reg_t index = reg_t::Undef;
+                arg_t::memory_t::scale_t scale = arg_t::memory_t::scale_t::S0;
+                if (OptionParserResult<monostate> c = consume_prefix_str(b.value().tail, " + ", monostate())) {
+                    if (OptionParserResult<reg_t> d = parse_register(c.value().tail)) {
+                        index = d.value().data;
+                        b.value().tail = d.value().tail;
 
-                // Parse a memory operand
-                if (OptionParserResult<monostate> a = consume_prefix_char(tail, '[', monostate())) {
-                    if (OptionParserResult<reg_t> b = parse_register(a.value().tail)) {
-                        reg_t base = b.value().data;
-
-                        // Maybe parse an index with scale
-                        reg_t index = reg_t::Undef;
-                        arg_t::memory_t::scale_t scale = arg_t::memory_t::scale_t::S0;
-                        if (OptionParserResult<monostate> c = consume_prefix_str(b.value().tail, " + ", monostate())) {
-                            if (OptionParserResult<reg_t> d = parse_register(c.value().tail)) {
-                                index = d.value().data;
-                                b.value().tail = d.value().tail;
-
-                                if (OptionParserResult<monostate> e = consume_prefix_str(d.value().tail, " * ",
-                                                                                         monostate())) {
-                                    if (OptionParserResult<i64> f = parse_i64(e.value().tail)) {
-                                        switch (f.value().data) {
-                                            case 0:
-                                                scale = arg_t::memory_t::scale_t::S0;
-                                                break;
-                                            case 1:
-                                                scale = arg_t::memory_t::scale_t::S1;
-                                                break;
-                                            case 2:
-                                                scale = arg_t::memory_t::scale_t::S2;
-                                                break;
-                                            case 4:
-                                                scale = arg_t::memory_t::scale_t::S4;
-                                                break;
-                                            case 8:
-                                                scale = arg_t::memory_t::scale_t::S8;
-                                                break;
-                                        }
-
-                                        b.value().tail = f.value().tail;
-                                    }
+                        if (OptionParserResult<monostate> e = consume_prefix_str(d.value().tail, " * ",
+                                                                                 monostate())) {
+                            if (OptionParserResult<i64> f = parse_i64(e.value().tail)) {
+                                switch (f.value().data) {
+                                    case 0:
+                                        scale = arg_t::memory_t::scale_t::S0;
+                                        break;
+                                    case 1:
+                                        scale = arg_t::memory_t::scale_t::S1;
+                                        break;
+                                    case 2:
+                                        scale = arg_t::memory_t::scale_t::S2;
+                                        break;
+                                    case 4:
+                                        scale = arg_t::memory_t::scale_t::S4;
+                                        break;
+                                    case 8:
+                                        scale = arg_t::memory_t::scale_t::S8;
+                                        break;
+                                    default:
+                                        throw logic_error("scale should be one of: 0 1 2 4 8");
                                 }
+
+                                b.value().tail = f.value().tail;
                             }
                         }
-
-                        i32 disp = 0;
-                        if (OptionParserResult<monostate> c = consume_prefix_str(b.value().tail, " + ", monostate())) {
-                            if (OptionParserResult<i64> d = parse_i64(c.value().tail)) {
-                                disp = i32(d.value().data);
-                                b.value().tail = d.value().tail;
-                            }
-                        }
-
-                        if (OptionParserResult<monostate> c = consume_prefix_char(b.value().tail, ']', monostate())) {
-                            arg_t result = arg_t::mem(base, index, scale, disp);
-                            return make_option(ParserResult(c.value().tail, result));
-                        } else {
-                            return OptionParserResult<arg_t>();
-                        }
-                    } else {
-                        return OptionParserResult<arg_t>();
                     }
+                }
+
+                i32 disp = 0;
+                if (OptionParserResult<monostate> c = consume_prefix_str(b.value().tail, " + ", monostate())) {
+                    if (OptionParserResult<i64> d = parse_i64(c.value().tail)) {
+                        disp = i32(d.value().data);
+                        b.value().tail = d.value().tail;
+                    }
+                }
+
+                if (OptionParserResult<monostate> c = consume_prefix_char(b.value().tail, ']', monostate())) {
+                    arg_t result = arg_t::mem(base, index, scale, disp);
+                    return make_option(ParserResult(c.value().tail, result));
                 } else {
                     return OptionParserResult<arg_t>();
                 }
-            });
-
-    return result;
+            } else {
+                return OptionParserResult<arg_t>();
+            }
+        } else {
+            return OptionParserResult<arg_t>();
+        }
+    });
 }
 
 static auto parse_line(strive tail) -> OptionParserResult<mnemo_t> {
@@ -210,91 +187,81 @@ static auto parse_line(strive tail) -> OptionParserResult<mnemo_t> {
         // Skip spaces
         ParserResult<monostate> b = skip_while_char(a.value().tail, [](char c) { return c == ' '; });
 
+        mnemo_t::width_t width = mnemo_t::width_t::NotSet;
         // Parse mnemo width
         if (OptionParserResult<mnemo_t::width_t> c = parse_mnemo_width(b.tail)) {
-            mnemo_t::width_t width = c.value().data;
+            width = c.value().data;
+            b.tail = c.value().tail;
+        }
 
-            // Skip spaces
-            ParserResult<monostate> d = skip_while_char(c.value().tail, [](char c) { return c == ' '; });
+        // Skip spaces
+        ParserResult<monostate> c = skip_while_char(b.tail, [](char c) { return c == ' '; });
 
-            arg_t arg1{};
-            // Maybe parse arg1
-            if (OptionParserResult<mnemo_t::arg_t> e = parse_arg(d.tail)) {
-                arg1 = e.value().data;
+        arg_t arg1{};
+        arg_t arg2{};
+        // Maybe parse arg1
+        if (OptionParserResult<mnemo_t::arg_t> d = parse_arg(c.tail)) {
+            arg1 = d.value().data;
+            c.tail = d.value().tail;
 
-                // Skip ", "
-                if (OptionParserResult<monostate> f = consume_prefix_str(e.value().tail, ", ", monostate())) {
-                    arg_t arg2{};
-                    // Maybe parse arg2
-                    if (OptionParserResult<mnemo_t::arg_t> g = parse_arg(f.value().tail)) {
-                        arg2 = g.value().data;
-
-                        // todo: consume \n
-
-                        mnemo_t mnemo = {
-                                .tag = tag,
-                                .width = width,
-                                .a1 = arg1,
-                                .a2 = arg2,
-                        };
-
-                        return make_option(ParserResult(g.value().tail, mnemo));
-                    } else {
-                        return OptionParserResult<mnemo_t>();
-                    }
-                } else {
-                    return OptionParserResult<mnemo_t>();
+            // Skip ", "
+            if (OptionParserResult<monostate> e = consume_prefix_str(d.value().tail, ", ", monostate())) {
+                // Maybe parse arg2
+                if (OptionParserResult<mnemo_t::arg_t> f = parse_arg(e.value().tail)) {
+                    arg2 = f.value().data;
+                    c.tail = f.value().tail;
                 }
-            } else {
-                return OptionParserResult<mnemo_t>();
             }
+        }
+
+        // Consume a newline
+        if (OptionParserResult<monostate> d = consume_prefix_char(c.tail, '\n', monostate())) {
+            mnemo_t mnemo = {
+                    .tag = tag,
+                    .width = width,
+                    .a1 = arg1,
+                    .a2 = arg2,
+            };
+
+            return make_option(ParserResult(d.value().tail, mnemo));
         } else {
-            return OptionParserResult<mnemo_t>();
+            throw logic_error("Uh oh");
         }
     } else {
         return OptionParserResult<mnemo_t>();
     }
 }
 
-namespace assembly {
-    namespace parse {
-        auto parse(strive tail) -> vector<mnemo_t> {
-            // Parses multiline assembly text
+namespace assembly::parse {
+    auto parse(strive tail) -> vector<mnemo_t> {
+        // Parses multiline assembly text
 
-            vector<mnemo_t> result{};
+        vector<mnemo_t> result{};
 
-            for (;;) {
-                if (OptionParserResult<mnemo_t> result1 = parse_line(tail)) {
-                    // If result is available, continue iteration
-                    tail = result1.value().tail;
-                    result.push_back(result1.value().data);
-
-                    // Consume a newline
-                    if (OptionParserResult<monostate> result2 = consume_prefix_char(tail, '\n', monostate())) {
-                        // If result is available, continue iteration
-                        tail = result2.value().tail;
-                    } else {
-                        // Newline not found
-                        throw logic_error("Uh oh");
-                    }
-                } else {
-                    // Else break
-                    break;
-                }
+        for (;;) {
+            if (OptionParserResult<mnemo_t> result1 = parse_line(tail)) {
+                // If result is available, continue iteration
+                tail = result1.value().tail;
+                result.push_back(result1.value().data);
+            } else {
+                // Else break
+                break;
             }
-
-            return result;
         }
 
-        auto test() -> void {
-            string s = "mov DWORD eax, 100\n"
-                       "mov BYTE ah, [eax + ebx * 2 + 128]\n";
-            auto mnemos = assembly::parse::parse(s);
+        return result;
+    }
 
-            for (auto &mnemo : mnemos) {
-                mnemo.print();
-                cout << "\n";
-            }
+    auto test() -> void {
+        string s = "mov DWORD eax, 100\n"
+                   "mov BYTE ah, [eax + ebx * 2 + 128]\n"
+                   "push DWORD [eax]\n"
+                   "ret\n";
+        auto mnemos = assembly::parse::parse(s);
+
+        for (auto &mnemo : mnemos) {
+            mnemo.print();
+            cout << "\n";
         }
     }
 }
