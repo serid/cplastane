c2794fb54a281574525f27d05013946123df8012
Sergey <serridd@gmail.com>
2021-04-24 16:05:22 +0300

    eval().

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 #include "tests/assembly.hxx"
 #include "parsec/tests/tests.hxx"
 #include "assembly/parse/parse.hxx"
+#include "jit/jit.hxx"
+
+auto eval(strive s) -> i64 {
+    auto mnemos = assembly::parse::parse(s);
+    auto bytes = assembly::assemble(mnemos);
+    return jit::eval_mc(bytes.data(), bytes.size());
+}
 
 int main() {
 //    syntax::parse::test_parser();
